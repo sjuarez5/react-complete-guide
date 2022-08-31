cbf0351e153214da0fc3d542937f6c2aff65e273
Samuel Juarez-Solis <sjuarez-solis@catalyte.io>
2022-08-31 12:04:36 -0700

    add handlers to pass data from child to parent components

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -4,9 +4,18 @@ import ExpenseForm from "./ExpenseForm.js";
 import "./NewExpense.css";
 
 const NewExpense = () => {
-  return (
+    const saveExpenseDataHandler = (enteredExpenseData) => {
+        const expenseData = {
+            ...enteredExpenseData,
+            id: Math.random().toString()
+        };
+
+        console.log(expenseData);
+    };
+
+    return (
     <div className="new-expense">
-      <ExpenseForm />
+      <ExpenseForm onSaveExpenseData = {saveExpenseDataHandler}/>
     </div>
   );
 };
