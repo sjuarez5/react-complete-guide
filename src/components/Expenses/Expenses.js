7bbfa0588c2b7edb0503b3fb652922cf0997b3c5
Samuel Juarez-Solis <sjuarez-solis@catalyte.io>
2022-09-01 08:49:36 -0700

    fix error with letter causing app to crash

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -6,7 +6,7 @@ import ExpensesList from "./ExpensesList.js";
 import "./Expenses.css";
 import "./ExpensesFilter.css";
 
-const Expenses = (props) => {F
+const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2020");
 
   const filterChangeHandler = (selectedYear) => {
